12215d5976da38e2876d25c3e21f475752ec8325
cbj252 <cbj252@gmail.com>
2021-07-20 10:32:55 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const projectBoxes = function(gridName) {
+  const mainImg = document.querySelector(gridName);
+  const projectImg = document.querySelector(`${gridName} > .projectDescription`);
+  const projectButton = document.querySelector(`${gridName} .viewProjectButton`);
+  const projectFullDetails = document.querySelector(
+    `${gridName} > .projectDescription > .projectDetails`
+  );
+  const projectFullDetailsRemove = document.querySelector(
+    `${gridName} .removeDescription`
+  );
+
+  mainImg.addEventListener("mouseover", () => {
+    projectButton.style.visibility = "visible";
+  });
+
+  mainImg.addEventListener("mouseout", () => {
+    projectButton.style.visibility = "hidden";
+  });
+
+  projectButton.addEventListener("click", () => {
+    projectImg.style.visibility = "visible";
+  });
+
+  projectFullDetailsRemove.addEventListener("click", () => {
+    projectImg.style.visibility = "hidden";
+  });
+}
+
+projectBoxes(".grid11");
+projectBoxes(".grid12");
